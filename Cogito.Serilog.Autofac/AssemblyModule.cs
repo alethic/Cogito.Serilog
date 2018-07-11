237c3143a97ae0723c2e58e06482264a8d30028b
Jerome Haltom <jhaltom@fileandserve.com>
2018-07-10 19:31:34 -0500

    Add registration.

---
+++ Cogito.Serilog.Autofac/AssemblyModule.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+using Autofac;
+using Autofac.Core;
+using Autofac.Core.Activators.Reflection;
+
+using Cogito.Autofac;
+
+using Serilog;
+
+namespace Cogito.Serilog.Autofac
+{
+
+    /// <summary>
+    /// Provides typed instances of the Serilog Logger interface to components.
+    /// </summary>
+    public class AssemblyModule :
+        Module
+    {
+
+        const string TargetTypeParameterName = "Autofac.AutowiringPropertyInjector.InstanceType";
+
+        /// <summary>
+        /// Invoked to load the module.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterFromAttributes(typeof(AssemblyModule).Assembly);
+            builder.Register(ctx => ctx.Resolve<ILoggerConfigurationBuilder>().BuildConfiguration()).SingleInstance();
+
+            // root logger instance
+            builder.Register((c, p) => c.ResolveOptional<LoggerConfiguration>()?.CreateLogger() ?? Log.Logger)
+                .Named<ILogger>("Root")
+                .SingleInstance();
+
+            // register actual provider for logger instances
+            builder.Register((c, p) =>
+            {
+                // find root logger
+                var _logger = c.ResolveNamed<ILogger>("Root");
+
+                var targetType = p.OfType<NamedParameter>()
+                    .FirstOrDefault(np => np.Name == TargetTypeParameterName && np.Value is Type);
+                if (targetType != null)
+                    return _logger.ForContext((Type)targetType.Value);
+
+                return _logger;
+            })
+            .As<ILogger>()
+            .ExternallyOwned();
+        }
+
+        protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry, IComponentRegistration registration)
+        {
+            // ignore components that provide loggers
+            if (registration.Services.OfType<TypedService>().Any(ts => ts.ServiceType == typeof(ILogger)))
+                return;
+
+            if (registration.Activator is ReflectionActivator ra)
+            {
+                // find ctors that accept a logger
+                var any = ra.ConstructorFinder
+                    .FindConstructors(ra.LimitType)
+                    .SelectMany(ctor => ctor.GetParameters())
+                    .Any(pi => pi.ParameterType == typeof(ILogger));
+
+                // no ctors found
+                if (!any)
+                    return;
+
+                // attach event to inject logger
+                registration.Preparing += (sender, args) =>
+                {
+                    // discover context logger instance from registered logger
+                    var logger = args.Context.Resolve<ILogger>()?.ForContext(registration.Activator.LimitType);
+                    if (logger == null)
+                        throw new NullReferenceException();
+
+                    // append logger parameter
+                    args.Parameters = new[] { TypedParameter.From(logger) }.Concat(args.Parameters);
+                };
+            }
+        }
+
+    }
+
+}
