066fc31ddd15598f8f3e13083859a568461d057e
Jerome Haltom <wasabi@cogito.cx>
2018-07-12 22:17:34 -0500

    Gain some default JSON destructuring policies.

---
+++ Cogito.Serilog.Autofac/Destructuring/JArrayDestructuringPolicy.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+using Cogito.Autofac;
+
+using Newtonsoft.Json.Linq;
+
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Cogito.Serilog.Autofac.Destructuring
+{
+
+    [RegisterAs(typeof(IDestructuringPolicy))]
+    public class JArrayDestructuringPolicy :
+        IDestructuringPolicy
+    {
+
+        public bool TryDestructure(object value, ILogEventPropertyValueFactory propertyValueFactory, out LogEventPropertyValue result)
+        {
+            if (value is JArray json)
+            {
+                result = propertyValueFactory.CreatePropertyValue(json.ToArray(), true);
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+
+    }
+
+}
---
+++ Cogito.Serilog.Autofac/Destructuring/JValueDestructuringPolicy.cs
@@ -0,0 +1,30 @@
+﻿using Cogito.Autofac;
+
+using Newtonsoft.Json.Linq;
+
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Cogito.Serilog.Autofac.Destructuring
+{
+
+    [RegisterAs(typeof(IDestructuringPolicy))]
+    public class JValueDestructuringPolicy :
+        IDestructuringPolicy
+    {
+
+        public bool TryDestructure(object value, ILogEventPropertyValueFactory propertyValueFactory, out LogEventPropertyValue result)
+        {
+            if (value is JValue json)
+            {
+                result = propertyValueFactory.CreatePropertyValue(json.Value, true);
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+
+    }
+
+}
